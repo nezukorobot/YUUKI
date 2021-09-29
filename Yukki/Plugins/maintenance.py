d36754bd2ec51a59a1d39c09642b8add3a25bf12
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-09-29 18:27:47 +0530

    Update maintenance.py

--- Yukki/Plugins/maintenance.py
+++ Yukki/Plugins/maintenance.py
@@ -5,9 +5,9 @@ from Yukki.YukkiUtilities.database.onoff import (is_on_off, add_on, add_off)
 from ..YukkiUtilities.helpers.filters import command
 
 
-@Client.on_message(command("Yukkip") & filters.user(SUDOERS))
+@Client.on_message(command("Rikudop") & filters.user(SUDOERS))
 async def smex(_, message):
-    usage = "**Usage:**\n/Yukkip [enable|disable]"
+    usage = "**Usage:**\n/Rikudop [enable|disable]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     chat_id = message.chat.id
@@ -16,7 +16,7 @@ async def smex(_, message):
     if state == "enable":
         user_id = 1
         await add_on(user_id)
-        await message.reply_text("Yukki Enabled for Maintenance")
+        await message.reply_text("Rikudo Senin Enabled for Maintenance")
     elif state == "disable":
         user_id = 1
         await add_off(user_id)
@@ -25,9 +25,9 @@ async def smex(_, message):
         await message.reply_text(usage)
 
         
-@Client.on_message(command("stp") & filters.user(SUDOERS))
+@Client.on_message(command("stpp") & filters.user(SUDOERS))
 async def sls_skfs(_, message):
-    usage = "**Usage:**\n/st [enable|disable]"
+    usage = "**Usage:**\n/stpp [enable|disable]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     chat_id = message.chat.id
