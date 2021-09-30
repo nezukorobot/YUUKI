95a70574480a2e60c013a74481366465718beb67
xxMUNNAxx <82759594+xxMUNNAxx@users.noreply.github.com>
2021-09-30 16:28:43 +0530

    Update lyrics.py

--- Yukki/Plugins/lyrics.py
+++ Yukki/Plugins/lyrics.py
@@ -67,7 +67,7 @@ async def lrsearch(_, message: Message):
     if S is None:
         return await m.edit("Lyrics not found :p")
     xxx = f"""
-**Lyrics Search Powered By Rikudo Senin Bot**
+**Lyrics Search Powered By MUNNA X MUSIC Bot**
 
 **Searched Song:-** __{query}__
 
